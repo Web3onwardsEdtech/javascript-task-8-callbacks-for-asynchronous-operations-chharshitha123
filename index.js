ab4ad9378b50b586c1e90712fd68b80803c0e463
Harshitha <chharshitha277@gmail.com>
2024-10-05 18:02:42 +0530

    "task 8 js backend development completed"

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+function delayedExecution(callback, delay, ...params) {
+
+    setTimeout(() => {
+        callback(...params); 
+    }, delay);
+}
+
+
+const logDelayedMessage = () => {
+    console.log("This is a delayed message.");
+};
+
+delayedExecution(logDelayedMessage, 1000);
+
+
+const logSum = (a, b) => {
+    console.log(a + b);
+};
+
+delayedExecution(logSum, 3000, 5, 10);
+
+
+const logCurrentTime = () => {
+    const now = new Date();
+    const currentTime = now.toLocaleTimeString(); 
+    console.log(currentTime);
+};
+
+delayedExecution(logCurrentTime, 5000);
+
